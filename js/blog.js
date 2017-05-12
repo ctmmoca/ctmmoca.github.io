a3b4ed04ebcd2416ab907489e94a8441e93198fa
Efrain J. Valentin Gonzalez <support@addict.hol.es>
2017-05-11 22:47:06 -0400

    c

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,7 @@ $(function() {
 	Parse.$ = jQuery;
 
 	// Replace this line with the one on your Quickstart Guide Page
-	Parse.initialize("HC87tn6aA7c3sYx9X0vwwLVXeqHDRMYYmrUBK5zv", "3piNGGnRMhvWo8u9pKD9TDc1MJlWhlvK78Vr3fHo");
+	Parse.initialize("k0OSMy6nTSCIigbMI8OGbiuE60ab8txGnuJnh5Fp", "6isNrwzGLeYwlxl3C80NErxRfe1cHXWexgm0vtGC");
 	Parse.initialize("CTM Moca"); // Your App Name
 	Parse.serverURL = 'https://parseapi.back4app.com/'; // Your Server URL
 
